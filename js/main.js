fae94505902e514d130b6abf8e30fdad8efacd35
robert <robert07spiridon@gmail.com>
2022-10-24 00:59:39 +0300

    dashboard v1

--- js/main.js
+++ js/main.js
@@ -1,87 +1,94 @@
 fetch('./data.json')
-.then((response)=>response.json())
-.then((json)=>{
-    localStorage.setItem('list1',JSON.stringify(json));
-    function createItem(object){
-        const item=document.createElement('div');
-        item.classList.add('box');
-        const anchor=document.createElement('a');
-        anchor.setAttribute('href','./pag.html?id='+object.id)
-        item.appendChild(anchor);
-        const img=document.createElement('img');
-        img.setAttribute('src',object.logo);
-        img.setAttribute('alt',object.company);
-        anchor.appendChild(img);
-        const time1=document.createElement('p');
-        time1.textContent=object.postedAt + ' • ' + object.contract ;
-        anchor.appendChild(time1);
-        const time2=document.createElement('p');
-        const post=document.createElement('h3');
-        post.textContent=object.position;
-        anchor.appendChild(post);
-        const company=document.createElement('p');
-        company.textContent=object.company;
-        anchor.appendChild(company);
-        const location=document.createElement('h4');
-        location.textContent=object.location;
-        anchor.appendChild(location);
-        return item;
-    
-    }
-function populate(countItem,lista){
-    const content =document.getElementById('content');
-    for(let i=0;i<countItem;i++)
-    {
-        const item1=createItem(lista[i]);
-      content.appendChild(item1);
-    }
-}
-function clean(){
-    const content =document.getElementById('content');
-    content.textContent='';
-}
-populate(6,json);
-const footerButton=document.getElementById('footerButton');
-footerButton.addEventListener('click',()=>{
-    clean();
-    populate(json.length,json);
-})
+    .then((response) => response.json())
+    .then((json) => {
+        localStorage.setItem('list1', JSON.stringify(json));
+        function createItem(object) {
+            const item = document.createElement('div');
+            item.classList.add('box');
+            const anchor = document.createElement('a');
+            anchor.setAttribute('href', './pag.html?id=' + object.id)
+            item.appendChild(anchor);
+            const img = document.createElement('img');
+            img.setAttribute('src', object.logo);
+            img.setAttribute('alt', object.company);
+            anchor.appendChild(img);
+            const time1 = document.createElement('p');
+            time1.textContent = object.postedAt + ' • ' + object.contract;
+            anchor.appendChild(time1);
+            const time2 = document.createElement('p');
+            const post = document.createElement('h3');
+            post.textContent = object.position;
+            anchor.appendChild(post);
+            const company = document.createElement('p');
+            company.textContent = object.company;
+            anchor.appendChild(company);
+            const location = document.createElement('h4');
+            location.textContent = object.location;
+            anchor.appendChild(location);
+            return item;
 
-const headerButton=document.getElementById('headerButton');
-headerButton.addEventListener('click',()=>{
-    const inputSearch=document.getElementById('search');
-    const inputLocation=document.getElementById('inputLocation');
-    const filter=json.filter((obj)=>{
-        return obj.position.toLowerCase().includes(inputSearch.value.toLowerCase()) && obj.location.toLowerCase().includes(inputLocation.value.toLowerCase());
+        }
+        function populate(countItem, lista) {
+            const content = document.getElementById('content');
+            for (let i = 0; i < countItem; i++) {
+                const item1 = createItem(lista[i]);
+                content.appendChild(item1);
+            }
+        }
+        function clean() {
+            const content = document.getElementById('content');
+            content.textContent = '';
+        }
+        populate(6, json);
+        const footerButton = document.getElementById('footerButton');
+        footerButton.addEventListener('click', () => {
+            clean();
+            populate(json.length, json);
+        })
+
+        const headerButton = document.getElementById('headerButton');
+        headerButton.addEventListener('click', () => {
+            const inputSearch = document.getElementById('search');
+            const inputLocation = document.getElementById('inputLocation');
+            const filter = json.filter((obj) => {
+                return obj.position.toLowerCase().includes(inputSearch.value.toLowerCase()) && obj.location.toLowerCase().includes(inputLocation.value.toLowerCase());
+            });
+
+
+            clean();
+            populate(filter.length, filter);
+            const navCheck = document.getElementById('navCheck');
+            if (navCheck.checked && inputSearch.value === '' && inputLocation.value === '') {
+                const filter1 = json.filter((obj) => {
+                    return obj.contract.toLowerCase().includes('full');
+
+
+                })
+                clean();
+                populate(filter1.length, filter1);
+            }
+            else if (navCheck.checked && inputSearch.value != '') {
+
+                const filter2 = json.filter((obj) => {
+                    return obj.contract.toLowerCase().includes('full') && obj.position.toLowerCase().includes(inputSearch.value.toLowerCase()) && obj.location.toLowerCase().includes(inputLocation.value.toLowerCase());
+                })
+                clean();
+                populate(filter2.length, filter2);
+            }
+            else if (navCheck.checked && inputLocation.value != '') {
+                const filter3 = json.filter((obj) => {
+                    return obj.contract.toLowerCase().includes('full') && obj.location.toLowerCase().includes(inputLocation.value.toLowerCase());
+                })
+                clean();
+                populate(filter3.length, filter3);
+
+            }
+
+
+        })
+
+
     });
-    
-    
-    clean();
-    populate(filter.length,filter);    
-    const navCheck=document.getElementById('navCheck');
-navCheck.addEventListener('change',(e)=>{
-   
-    const filter1=json.filter((obj)=>{
-        return obj.contract.toLowerCase().includes('full') && obj.position.toLowerCase().includes(inputSearch.value.toLowerCase()) && obj.location.toLowerCase().includes(inputLocation.value.toLowerCase());
-       
-    })
-    if(e.target.checked)
-    {
-        clean();
-        populate(filter1.length,filter1);
-    }
-    else {
-        clean();
-        populate(6,json);
-        
-    }
-    
-})
-   
-})
-
-
-});
 
 
 
